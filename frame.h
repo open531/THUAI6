1e14676f840494de1924329182b96c0a1821905a
Xuc Pan <48503851+Panxuc@users.noreply.github.com>
2023-04-20 11:31:55 +0800

    Update frame.h

--- frame.h
+++ frame.h
@@ -1,5 +1,6 @@
 #include"AI.h"
 #include"API.h"
+#include<stack>
 
 class Point
 {
@@ -11,6 +12,297 @@ public:
 	int y;
 };
 
+class Node :virtual public Point
+{
+public:
+	Node(int x_ = 0, int y_ = 0, int px_ = 0, int py_ = 0,
+		float fc_ = FLT_MAX, float gc_ = FLT_MAX, float hc_ = FLT_MAX)
+		:Point(x, y), parentX(px_), parentY(py_), fCost(fc_), gCost(gc_), hCost(hc_) {};
+	Node(Point p, int px_ = 0, int py_ = 0,
+		float fc_ = FLT_MAX, float gc_ = FLT_MAX, float hc_ = FLT_MAX)
+		:Point(p.x, p.y), parentX(px_), parentY(py_), fCost(fc_), gCost(gc_), hCost(hc_) {};
+	int parentX;
+	int parentY;
+	float fCost;
+	float gCost;
+	float hCost;
+};
+
+static bool HasInitMap;
+static unsigned char Map[50][50];
+static unsigned char Access[50][50];
+static std::vector<Point> Gate;
+static std::vector<Point> Homework;
+
+void InitMap(IStudentAPI& api)
+{
+	int i, j;
+	for (i = 0; i < 50; i++)
+	{
+		for (j = 0; j < 50; j++)
+		{
+			Map[i][j] = (unsigned char)api.GetPlaceType(i, j);
+			switch (Map[i][j])
+			{
+			case 2U:	// Wall
+			case 6U:	// HiddenGate
+			case 11U:	// Chest
+				Access[i][j] = 0U;
+				break;
+			case 3U:	// Grass
+				Access[i][j] = 3U;
+				break;
+			case 7U:	// Window
+				Access[i][j] = 1U;
+				break;
+			case 8U:	// Door3
+			case 9U:	// Door5
+			case 10U:	// Door6
+				Access[i][j] = 2U;
+				break;
+			case 4U:	// ClassRoom
+				Access[i][j] = 0U;
+				Homework.emplace_back(Point(i, j));
+				break;
+			case 5U:	// Gate
+				Access[i][j] = 0U;
+				Gate.emplace_back(Point(i, j));
+				break;
+			default:
+				Access[i][j] = 2U;
+				break;
+			}
+		}
+	}
+}
+
+class AStar
+{
+public:
+	bool IsValidWithoutWindows(int x, int y) { return (bool)(Access[x][y] / 2); }
+	bool IsValidWithWindows(int x, int y) { return (bool)Access[x][y]; }
+	static bool IsDestination(int x, int y, Node dest)
+	{
+		if (x == dest.x && y == dest.y) return true; else return false;
+	}
+	double CalculateH(int x, int y, Node dest)
+	{
+		double H = (sqrt((x - dest.x) * (x - dest.x) + (y - dest.y) * (y - dest.y)));
+		return H;
+	}
+	std::vector<Node> MakePath(std::array<std::array<Node, 50>, 50> map, Node dest)
+	{
+		try
+		{
+			int x = dest.x;
+			int y = dest.y;
+			std::stack<Node> Path;
+			std::vector<Node> UsablePath;
+			while (!(map[x][y].parentX == x && map[x][y].parentY == y)
+				&& map[x][y].x != -1 && map[x][y].y != -1)
+			{
+				Path.push(map[x][y]);
+				int tempX = map[x][y].parentX;
+				int tempY = map[x][y].parentY;
+				x = tempX;
+				y = tempY;
+
+			}
+			Path.push(map[x][y]);
+			while (!Path.empty()) {
+				Node top = Path.top();
+				Path.pop();
+				UsablePath.emplace_back(top);
+			}
+			return UsablePath;
+		}
+		catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::vector<Node> AStarWithoutWindows(Node src, Node dest)
+	{
+		std::vector<Node> empty;
+		if (IsValidWithoutWindows(dest.x, dest.y) == false)
+		{
+			return empty;
+		}
+		if (IsDestination(src.x, src.y, dest))
+		{
+			return empty;
+		}
+		bool ClosedList[50][50];
+		std::array<std::array < Node, 50>, 50> AStarMap;
+		for (int x = 0; x < 50; x++) {
+			for (int y = 0; y < 50; y++) {
+				AStarMap[x][y].fCost = FLT_MAX;
+				AStarMap[x][y].gCost = FLT_MAX;
+				AStarMap[x][y].hCost = FLT_MAX;
+				AStarMap[x][y].parentX = -1;
+				AStarMap[x][y].parentY = -1;
+				AStarMap[x][y].x = x;
+				AStarMap[x][y].y = y;
+				ClosedList[x][y] = false;
+			}
+		}
+		int x = src.x;
+		int y = src.y;
+		AStarMap[x][y].fCost = 0.0;
+		AStarMap[x][y].gCost = 0.0;
+		AStarMap[x][y].hCost = 0.0;
+		AStarMap[x][y].parentX = x;
+		AStarMap[x][y].parentY = y;
+		std::vector<Node> OpenList;
+		OpenList.emplace_back(AStarMap[x][y]);
+		bool FoundDest = false;
+		while (!OpenList.empty() && OpenList.size() < 50 * 50) {
+			Node node;
+			do {
+				float temp = FLT_MAX;
+				std::vector<Node>::iterator itNode;
+				for (std::vector<Node>::iterator it = OpenList.begin();
+					it != OpenList.end(); it = next(it)) {
+					Node n = *it;
+					if (n.fCost < temp) {
+						temp = n.fCost;
+						itNode = it;
+					}
+				}
+				node = *itNode;
+				OpenList.erase(itNode);
+			} while (IsValidWithoutWindows(node.x, node.y) == false);
+			x = node.x;
+			y = node.y;
+			ClosedList[x][y] = true;
+			for (int newX = -1; newX <= 1; newX++) {
+				for (int newY = -1; newY <= 1; newY++) {
+					if (newX != 0 && newY != 0) continue;
+					double gNew, hNew, fNew;
+					if (IsValidWithoutWindows(x + newX, y + newY)) {
+						if (IsDestination(x + newX, y + newY, dest))
+						{
+							AStarMap[x + newX][y + newY].parentX = x;
+							AStarMap[x + newX][y + newY].parentY = y;
+							FoundDest = true;
+							return MakePath(AStarMap, dest);
+						}
+						else if (ClosedList[x + newX][y + newY] == false)
+						{
+							gNew = node.gCost + 1.0;
+							hNew = CalculateH(x + newX, y + newY, dest);
+							fNew = gNew + hNew;
+							if (AStarMap[x + newX][y + newY].fCost == FLT_MAX ||
+								AStarMap[x + newX][y + newY].fCost > fNew)
+							{
+								AStarMap[x + newX][y + newY].fCost = fNew;
+								AStarMap[x + newX][y + newY].gCost = gNew;
+								AStarMap[x + newX][y + newY].hCost = hNew;
+								AStarMap[x + newX][y + newY].parentX = x;
+								AStarMap[x + newX][y + newY].parentY = y;
+								OpenList.emplace_back(AStarMap[x + newX][y + newY]);
+							}
+						}
+					}
+				}
+			}
+		}
+		if (FoundDest == false) {
+			return empty;
+		}
+	}
+	std::vector<Node> AStarWithWindows(Node src, Node dest)
+	{
+		std::vector<Node> empty;
+		if (IsValidWithWindows(dest.x, dest.y) == false)
+		{
+			return empty;
+		}
+		if (IsDestination(src.x, src.y, dest))
+		{
+			return empty;
+		}
+		bool ClosedList[50][50];
+		std::array<std::array < Node, 50>, 50> AStarMap;
+		for (int x = 0; x < 50; x++) {
+			for (int y = 0; y < 50; y++) {
+				AStarMap[x][y].fCost = FLT_MAX;
+				AStarMap[x][y].gCost = FLT_MAX;
+				AStarMap[x][y].hCost = FLT_MAX;
+				AStarMap[x][y].parentX = -1;
+				AStarMap[x][y].parentY = -1;
+				AStarMap[x][y].x = x;
+				AStarMap[x][y].y = y;
+				ClosedList[x][y] = false;
+			}
+		}
+		int x = src.x;
+		int y = src.y;
+		std::cerr << x << ' ' << y;
+		AStarMap[x][y].fCost = 0.0;
+		AStarMap[x][y].gCost = 0.0;
+		AStarMap[x][y].hCost = 0.0;
+		AStarMap[x][y].parentX = x;
+		AStarMap[x][y].parentY = y;
+		std::vector<Node> OpenList;
+		OpenList.emplace_back(AStarMap[x][y]);
+		bool FoundDest = false;
+		while (!OpenList.empty() && OpenList.size() < 50 * 50) {
+			Node node;
+			do {
+				float temp = FLT_MAX;
+				std::vector<Node>::iterator itNode;
+				for (std::vector<Node>::iterator it = OpenList.begin();
+					it != OpenList.end(); it = next(it)) {
+					Node n = *it;
+					if (n.fCost < temp) {
+						temp = n.fCost;
+						itNode = it;
+					}
+				}
+				node = *itNode;
+				OpenList.erase(itNode);
+			} while (IsValidWithWindows(node.x, node.y) == false);
+			x = node.x;
+			y = node.y;
+			ClosedList[x][y] = true;
+			for (int newX = -1; newX <= 1; newX++) {
+				for (int newY = -1; newY <= 1; newY++) {
+					if (newX != 0 && newY != 0) continue;
+					double gNew, hNew, fNew;
+					if (IsValidWithWindows(x + newX, y + newY)) {
+						if (IsDestination(x + newX, y + newY, dest))
+						{
+							AStarMap[x + newX][y + newY].parentX = x;
+							AStarMap[x + newX][y + newY].parentY = y;
+							FoundDest = true;
+							return MakePath(AStarMap, dest);
+						}
+						else if (ClosedList[x + newX][y + newY] == false)
+						{
+							gNew = node.gCost + 1.0;
+							hNew = CalculateH(x + newX, y + newY, dest);
+							fNew = gNew + hNew;
+							if (AStarMap[x + newX][y + newY].fCost == FLT_MAX ||
+								AStarMap[x + newX][y + newY].fCost > fNew)
+							{
+								AStarMap[x + newX][y + newY].fCost = fNew;
+								AStarMap[x + newX][y + newY].gCost = gNew;
+								AStarMap[x + newX][y + newY].hCost = hNew;
+								AStarMap[x + newX][y + newY].parentX = x;
+								AStarMap[x + newX][y + newY].parentY = y;
+								OpenList.emplace_back(AStarMap[x + newX][y + newY]);
+							}
+						}
+					}
+				}
+			}
+		}
+		if (FoundDest == false) {
+			return empty;
+		}
+	}
+}astar;
+
 template<class IFooAPI>
 class Utilities
 {
@@ -19,49 +311,162 @@ private:
 public:
 	Utilities(IFooAPI api) : API(api) {}
 
-	// void Update(std::string info); ¸üĞÂµØÍ¼ĞÅÏ¢£¬±ÈÈçÃÅºÍÒş²ØĞ£ÃÅ£¬ĞèÒªÔ¼¶¨infoµÄ¸ñÊ½
-	void MoveTo(Point Dest); // ÍùÄ¿µÄµØ¶¯Ò»¶¯
+	// void Update(Point Door, bool State);			//æ›´æ–°åœ°å›¾ä¿¡æ¯ï¼Œæ¯”å¦‚é—¨å’Œéšè—æ ¡é—¨ï¼Œéœ€è¦çº¦å®šinfoçš„æ ¼å¼
+	void MoveTo(Point Dest, bool WithWindows);		// å¾€ç›®çš„åœ°åŠ¨ä¸€åŠ¨
 	// void MoveToAccurate(Point Dest);
-	bool NearPoint(Point P, int level = 1); // level=0ÅĞ¶Ïµ±Ç°ÊÇ·ñÔÚ¸Ã¸ñ×ÓÉÏ£¬1ÅĞ¶ÏÊÇ·ñÔÚ¸ñ×ÓÉÏ»òÖÜÎ§4¸ñ£¬2ÅĞ¶ÏÊÇ·ñÔÚ¸ñ×ÓÉÏ»òÖÜÎ§8¸ñ
-	void MoveToNearestGate(); // Íù×î½üµÄĞ£ÃÅÅÔ±ß¶¯Ò»¶¯
-	bool NearGate(); // ÒÑ¾­ÔÚĞ£ÃÅÅÔ±ßÁËÂğ£¿
-	void MoveToNearestHomework(); // Íù×î½üµÄ×÷ÒµµÄ·½Ïò¶¯Ò»¶¯
-	bool NearHomework(); // ÒÑ¾­ÔÚ×÷ÒµÅÔ±ßÁËÂğ£¿
-	int EstimateTime(Point Dest); // È¥Ä¿µÄµØµÄÔ¤¹ÀÊ±¼ä
+	bool NearPoint(Point P, int level = 1);
+	// level=0åˆ¤æ–­å½“å‰æ˜¯å¦åœ¨è¯¥æ ¼å­ä¸Šï¼Œ1åˆ¤æ–­æ˜¯å¦åœ¨æ ¼å­ä¸Šæˆ–å‘¨å›´4æ ¼ï¼Œ2åˆ¤æ–­æ˜¯å¦åœ¨æ ¼å­ä¸Šæˆ–å‘¨å›´8æ ¼
+	void MoveToNearestGate(bool WithWindows);		// å¾€æœ€è¿‘çš„æ ¡é—¨æ—è¾¹åŠ¨ä¸€åŠ¨
+	bool NearGate();								// å·²ç»åœ¨æ ¡é—¨æ—è¾¹äº†å—ï¼Ÿ
+	void MoveToNearestHomework(bool WithWindows);	// å¾€æœ€è¿‘çš„ä½œä¸šçš„æ–¹å‘åŠ¨ä¸€åŠ¨
+	bool NearHomework();							// å·²ç»åœ¨ä½œä¸šæ—è¾¹äº†å—ï¼Ÿ
+	int EstimateTime(Point Dest);					// å»ç›®çš„åœ°çš„é¢„ä¼°æ—¶é—´
 };
 
-/* ĞÅÏ¢Ğ­Òé
-ĞÅÏ¢Í·£¨info[0]£©
-MapUpdate µØÍ¼¸üĞÂ
+void Utilities<class IFooAPI>::MoveTo(Point Dest, bool WithWindows)
+{
+	int sx = API.GetSelfInfo()->x;
+	int sy = API.GetSelfInfo()->y;
+	std::vector<Node> UsablePath;
+	if (WithWindows) UsablePath = astar.AStarWithWindows(Node(sx / 1000, sy / 1000), Dest);
+	else UsablePath = astar.AStarWithoutWindows(Node(sx / 1000, sy / 1000), Dest);
+	if (UsablePath.size() < 2) return;
+	int tx, ty;
+	if (UsablePath.size() >= 3
+		&& astar.IsValidWithoutWindows(sx / 1000, sy / 1000)
+		&& astar.IsValidWithoutWindows(UsablePath[1].x, UsablePath[1].y)
+		&& astar.IsValidWithoutWindows(UsablePath[2].x, UsablePath[2].y)
+		&& astar.IsValidWithoutWindows(sx / 1000, UsablePath[2].y)
+		&& astar.IsValidWithoutWindows(UsablePath[2].x, sy / 1000))
+	{
+		tx = UsablePath[2].x * 1000 + 500;
+		ty = UsablePath[2].y * 1000 + 500;
+	}
+	else
+	{
+		tx = UsablePath[1].x * 1000 + 500;
+		ty = UsablePath[1].y * 1000 + 500;
+	}
+	int dx = tx - sx;
+	int dy = ty - sy;
+	if (Map[tx / 1000][ty / 1000] != 7U)
+	{
+		API.Move(1000 * sqrt(dx * dx + dy * dy) / API.GetSelfInfo()->speed, atan2(dy, dx));
+	}
+	else
+	{
+		API.SkipWindow();
+	}
+}
 
-TrickerInfo µ·µ°¹íĞÅÏ¢
+bool Utilities<class IFooAPI>::NearPoint(Point P, int level = 1)
+{
+	Point Self(API.GetSelfInfo()->x / 1000, API.GetSelfInfo()->y / 1000);
+	switch (level)
+	{
+	case 0:
+		return (P.x == Self.x && P.y == Self.y) ? true : false;
+		break;
+	case 1:
+		return (abs(P.x - Self.x) + abs(P.y - Self.y) <= 1) ? true : false;
+		break;
+	case 2:
+		return (abs(P.x - Self.x) <= 1 && abs(P.y - Self.y) <= 1) ? true : false;
+		break;
+	}
+}
 
-NeedHelp ÇëÇóÖ§Ô®
-	info[1-2]£º×ø±ê
-WantTool ÇëÇó»ñÈ¡µÀ¾ß£¨¿ÉÄÜĞèÒª·´À¡£¿²»È»Ì«Ô¶ÁËÅÜÈ¥²»»®Ëã£©
+void Utilities<class IFooAPI>::MoveToNearestGate(bool WithWindows)
+{
+	int minDistance = INT_MAX;
+	int minNum = 0;
+	int Distance = INT_MAX;
+	Point Self(API.GetSelfInfo()->x / 1000, API.GetSelfInfo()->y / 1000);
+	for (int i = 0; i < Gate.size(); i++)
+	{
+		Distance = astar.AStarWithWindows(Self, Gate[i]).size();
+		if (Distance < minDistance)
+		{
+			minDistance = Distance;
+			minNum = i;
+		}
+	}
+	MoveTo(Gate[minNum], WithWindows);
+}
+
+bool Utilities<class IFooAPI>::NearGate()
+{
+	for (int i = 0; i < Gate.size(); i++)
+	{
+		if (NearPoint(Gate[i], 2)) return true;
+	}
+	return false;
+}
+
+void Utilities<class IFooAPI>::MoveToNearestHomework(bool WithWindows)
+{
+	int minDistance = INT_MAX;
+	int minNum = 0;
+	int Distance = INT_MAX;
+	Point Self(API.GetSelfInfo()->x / 1000, API.GetSelfInfo()->y / 1000);
+	for (int i = 0; i < Homework.size(); i++)
+	{
+		Distance = astar.AStarWithWindows(Self, Homework[i]).size();
+		if (Distance < minDistance)
+		{
+			minDistance = Distance;
+			minNum = i;
+		}
+	}
+	MoveTo(Homework[minNum], WithWindows);
+}
+
+bool Utilities<class IFooAPI>::NearHomework()
+{
+	for (int i = 0; i < Homework.size(); i++)
+	{
+		if (NearPoint(Homework[i], 2)) return true;
+	}
+	return false;
+}
+
+int Utilities<class IFooAPI>::EstimateTime(Point Dest)
+{
+	Point Self(API.GetSelfInfo()->x / 1000, API.GetSelfInfo()->y / 1000);
+	int Distance = astar.AStarWithWindows(Self, Dest).size();
+	int Speed = API.GetSelfInfo()->speed;
+	int Time = Distance * 1000 / Speed;
+	return Time;
+}
+
+/* ä¿¡æ¯åè®®
+ä¿¡æ¯å¤´ï¼ˆinfo[0]ï¼‰
+MapUpdate åœ°å›¾æ›´æ–°
+TrickerInfo æ£è›‹é¬¼ä¿¡æ¯
+NeedHelp è¯·æ±‚æ”¯æ´
+	info[1-2]ï¼šåæ ‡
+WantTool è¯·æ±‚è·å–é“å…·ï¼ˆå¯èƒ½éœ€è¦åé¦ˆï¼Ÿä¸ç„¶å¤ªè¿œäº†è·‘å»ä¸åˆ’ç®—ï¼‰
 */
 #define MapUpdate 0x01
 #define TrickerInfo 0x02
 #define NeedHelp 0x03
 #define WantTool 0x04
 
-/* ÈËÎï×´Ì¬
-CantMove ¶¯µ¯²»µÃ£¨³ÁÃÔ/Ç°ºóÒ¡/±ÏÒµ¡­¡­£©
-Default Ä¬ÈÏ
-DoHomework È¥Ğ´×÷Òµ/ÔÚĞ´×÷Òµ
-OpenGate È¥¿ªĞ£ÃÅ/ÔÚ¿ªĞ£ÃÅ
-OpenChest È¥¿ªÏä×Ó/ÔÚ¿ªÏä×Ó
-Danger Ó¦¶ÔÎ£ÏÕ
-Rousing »½ĞÑÄ³ÈË
-Encouraging ÃãÀøÄ³ÈË
-Picking È¥¼ñµÀ¾ß
-ÒÔÉÏÊÇËùÓĞ½ÇÉ«ÆÕ±éÓµÓĞµÄ×´Ì¬£¬ÆäËû×´Ì¬ĞèÒª×ÔĞĞ¶¨Òå£¬½¨Òé´Ó0x80¿ªÊ¼
-±ÈÈçÔË¶¯Ô±¿ÉÒÔ¶¨ÒåÒ»¸ö×´Ì¬½ĞÕıÃæÓ²¸Õ£¬ÀÏÊ¦¿ÉÒÔ¶¨ÒåÒ»¸ö×´Ì¬½ĞÑ²Âß£¨ÊÔÍ¼ÔÚÊÓÒ°ÄÚ¸ú×Åµ·µ°¹í£©£¬µÈµÈ
-»ù±¾Âß¼­¾ÍÊÇÃ¿´ÎAI::Play¶¼»á¸ù¾İĞÅÏ¢À´¾ö¶¨Î¬³Öµ±Ç°×´Ì¬»¹ÊÇÌøµ½ÁíÒ»¸ö×´Ì¬£¨ÀàËÆÓÚÍ¼Áé»úÄ£ÄâË¼Î¬£©
-
-ĞèÒªÒ»Ğ©¿Õ¼ä´¢´æ±ØÒªµÄĞÅÏ¢£¬¿ÉÄÜ»áÓÃµ½Í¨ĞÅ
-
-»òĞí¿ÉÒÔ¸ø¸÷ÖÖ×´Ì¬·Ö¸öÀà»òÕßÓÅÏÈ¼¶£¬·½±ãĞ´´úÂë
+/* äººç‰©çŠ¶æ€
+CantMove åŠ¨å¼¹ä¸å¾—ï¼ˆæ²‰è¿·/å‰åæ‘‡/æ¯•ä¸šâ€¦â€¦ï¼‰
+Default é»˜è®¤
+DoHomework å»å†™ä½œä¸š/åœ¨å†™ä½œä¸š
+OpenGate å»å¼€æ ¡é—¨/åœ¨å¼€æ ¡é—¨
+OpenChest å»å¼€ç®±å­/åœ¨å¼€ç®±å­
+Danger åº”å¯¹å±é™©
+Rousing å”¤é†’æŸäºº
+Encouraging å‹‰åŠ±æŸäºº
+Picking å»æ¡é“å…·
+ä»¥ä¸Šæ˜¯æ‰€æœ‰è§’è‰²æ™®éæ‹¥æœ‰çš„çŠ¶æ€ï¼Œå…¶ä»–çŠ¶æ€éœ€è¦è‡ªè¡Œå®šä¹‰ï¼Œå»ºè®®ä»0x80å¼€å§‹
+æ¯”å¦‚è¿åŠ¨å‘˜å¯ä»¥å®šä¹‰ä¸€ä¸ªçŠ¶æ€å«æ­£é¢ç¡¬åˆšï¼Œè€å¸ˆå¯ä»¥å®šä¹‰ä¸€ä¸ªçŠ¶æ€å«å·¡é€»ï¼ˆè¯•å›¾åœ¨è§†é‡å†…è·Ÿç€æ£è›‹é¬¼ï¼‰ï¼Œç­‰ç­‰
+åŸºæœ¬é€»è¾‘å°±æ˜¯æ¯æ¬¡AI::Playéƒ½ä¼šæ ¹æ®ä¿¡æ¯æ¥å†³å®šç»´æŒå½“å‰çŠ¶æ€è¿˜æ˜¯è·³åˆ°å¦ä¸€ä¸ªçŠ¶æ€ï¼ˆç±»ä¼¼äºå›¾çµæœºæ¨¡æ‹Ÿæ€ç»´ï¼‰
+éœ€è¦ä¸€äº›ç©ºé—´å‚¨å­˜å¿…è¦çš„ä¿¡æ¯ï¼Œå¯èƒ½ä¼šç”¨åˆ°é€šä¿¡
+æˆ–è®¸å¯ä»¥ç»™å„ç§çŠ¶æ€åˆ†ä¸ªç±»æˆ–è€…ä¼˜å…ˆçº§ï¼Œæ–¹ä¾¿å†™ä»£ç 
 */
 #define CantMove 0x00
 #define Default 0x10
